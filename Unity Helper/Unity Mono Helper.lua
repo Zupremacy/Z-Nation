ee1ab4b5b2989a5d0fdad2a618656331e31b16c0
Zed <a.bendiah@gmail.com>
2025-09-12 21:16:47 +0100

    Unity Mono Helper.lua

--- Unity Helper/Unity Mono Helper.lua
+++ Unity Helper/Unity Mono Helper.lua
@@ -1,17 +1,11 @@
--- Unity Mono Helper for Cheat Engine | By Zupremacy 
+-- Unity Mono Helper v1.1 for Cheat Engine | By Zupremacy 
 -- Params : Setup the values after your investigation of Mono Instances
--- Hotkey : Default hotkey is F5 you can change is or add multiple executions with different Hotkeys
+-- local methodArgs = {999} | It could be dictionnary of arguments  {***} to run the method
 
--- local assemblyName = "Assembly-CSharp"
--- local classNamespace = ""
--- local className = "WorldManager"
--- local staticFieldName = "_instance"
--- local methodName = "RPC_AddMoney" 	| While setting multiple methods you need to create multiple methodname fileds (LUA) 
--- local methodArgs = {999} 			| And this line also, you can setup a dictionnary of arguments  {***} to run your method
--- local methodReturnType = "System.Void"
--- local methodParamTypes = { "System.Int32" }
+-- Changelog : 1.1 More modulable and reusable code 
 
 -- I hope this will make it easy for you to hack your favorites Unity Games.
+-- More to come.
 
 -- Zed
 
@@ -20,19 +14,18 @@
 
 if syntaxcheck then return end
 
---Call Method
 function callMonoMethod(assemblyName, classNamespace, className, staticFieldName, methodName, methodArgs, methodReturnType, methodParamTypes)
   mono_initialize()
   LaunchMonoDataCollector()
 
   local domains = mono_enumDomains()
   if not domains or #domains == 0 then
-    print("[-] Mono domaines not found") return
+    print("[-] No domain found") return
   end
   local domain = domains[1]
   mono_setCurrentDomain(domain)
 
-  -- Find class
+  -- Find Class
   local classId = nil
   for _, asm in ipairs(mono_enumAssemblies()) do
     local img = mono_getImageFromAssembly(asm)
@@ -50,7 +43,7 @@ function callMonoMethod(assemblyName, classNamespace, className, staticFieldName
     print("[-] Class not found : " .. className) return
   end
 
-  -- Find static field + class ID
+  -- Find the static field "instance"
   local fields = mono_class_enumFields(classId)
   local offset = nil
   for _, f in ipairs(fields) do
@@ -63,7 +56,7 @@ function callMonoMethod(assemblyName, classNamespace, className, staticFieldName
     print("[-] Static field '" .. staticFieldName .. "' not found") return
   end
 
-  -- Get Instance adress
+  -- Get static instance adress
   local staticBase = mono_class_getStaticFieldAddress(domain, classId)
   local instAddr = readPointer(staticBase + offset)
   if not instAddr or instAddr == 0 then
@@ -73,63 +66,73 @@ function callMonoMethod(assemblyName, classNamespace, className, staticFieldName
   -- Check class type
   local actualClassId, classNameCheck = mono_object_getClass(instAddr)
   if actualClassId ~= classId then
-    print("[-] Unkown instance type (" .. tostring(classNameCheck) .. ")") return
+    print("[!] Warning : Instance is not a (" .. tostring(classNameCheck) .. ")")
+
   end
 
-  -- Find Method
-  local methodId = mono_class_findMethod(classId, methodName, #methodArgs, methodReturnType, table.unpack(methodParamTypes))
+  -- Find the method
+  local args = methodArgs or {}             -- safe if nil
+  local paramTypes = methodParamTypes or {} -- safe if nil
+  local methodId = mono_class_findMethod(classId, methodName, #args, methodReturnType, table.unpack(paramTypes))
   if not methodId then
     print("[-] Method '" .. methodName .. "' not found") return
   end
   mono_compile_method(methodId)
 
-  -- Invoke method
-  local result = mono_invoke_method(domain, methodId, instAddr, methodArgs)
-  print("[*] Method executed (retour = " .. tostring(result) .. ")")
+  -- call the method
+  local result = mono_invoke_method(domain, methodId, instAddr, args)
+  print("[*] " .. methodName .. " executed! (return = " .. tostring(result) .. ")")
 end
 
--- Params
-local assemblyName = "Assembly-CSharp"
-local classNamespace = ""
-local className = "WorldManager"
-local staticFieldName = "_instance"
-local methodName = "RPC_AddMoney"
-local methodArgs = {999}
-local methodReturnType = "System.Void"
-local methodParamTypes = { "System.Int32" }
-
-
-local methodName2 = "RPC_AddXP"
-local methodArgs2 = {25}
-
-
--- Hotkey : Execute method when F5 is down
-if f5hotkey then
-  f5hotkey.destroy()
-  f5hotkey = nil
-end
--- Hotkey : Creation of the Hotkey
-
+-- 🎯 Hotkeys
+if f5hotkey then f5hotkey.destroy() f5hotkey=nil end
 f5hotkey = createHotkey(function()
   callMonoMethod(
-    assemblyName,
-    classNamespace,
-    className,
-    staticFieldName,
-    methodName,
-    methodArgs,
-    methodReturnType,
-    methodParamTypes
+    "Assembly-CSharp",
+    "",
+    "WorldManager",
+    "_instance",
+    "RPC_AddMoney",
+    {99999},              -- args
+    "System.Void",        -- return type
+    {"System.Int32"}      -- param types
   )
 end, VK_F5)
+print("[*] Hotkey F5 : Add Money 99999$")
 
-print("[*] Hotkey F5 pressed : Invoke " .. methodName)
+if f6hotkey then f6hotkey.destroy() f6hotkey=nil end
+f6hotkey = createHotkey(function()
+  callMonoMethod(
+    "Assembly-CSharp",
+    "",
+    "WorldManager",
+    "_instance",
+    "RPC_AddXP",
+    {25},                 -- args
+    "System.Void",        -- return type
+    {"System.Int32"}      -- param types
+  )
+end, VK_F6)
+print("[*] Hotkey F6 : Add XP 25")
+
+if f7hotkey then f7hotkey.destroy() f7hotkey=nil end
+f7hotkey = createHotkey(function()
+  callMonoMethod(
+    "Assembly-CSharp",
+    "",
+    "WorldManager",
+    "_instance",
+    "RPC_ResetHealth",
+    nil,                  -- NO args
+    "System.Void",        -- return type
+    nil                   -- NO param types
+  )
+end, VK_F7)
+print("[*] Hotkey F7  : Reset Health")
+
 
 [disable]
-
 {$lua}
-if f5hotkey then
-  f5hotkey.destroy()
-  f5hotkey = nil
-  print("[*] Hotkey F5 released")
-end
+if f5hotkey then f5hotkey.destroy() f5hotkey=nil print("[-] Hotkey F5 deactivated") end
+if f6hotkey then f6hotkey.destroy() f6hotkey=nil print("[-] Hotkey F6 deactivated") end
+if f7hotkey then f7hotkey.destroy() f7hotkey=nil print("[-] Hotkey F7 deactivated") end
